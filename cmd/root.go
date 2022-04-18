223e196b45d81e237d0dcdee77681bb6b87859d0
Jason Lynch <jason.lynch@snyk.io>
2022-04-18 14:58:17 -0400

    [CLOUD-125] Combine iac and cloud commands

--- cmd/root.go
+++ cmd/root.go
@@ -1,15 +1,21 @@
 package cmd
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
+	"github.com/snyk/unified-policy-engine/pkg/data"
+	"github.com/snyk/unified-policy-engine/pkg/loader"
+	"github.com/snyk/unified-policy-engine/pkg/upe"
 	"github.com/spf13/cobra"
 )
 
 var (
 	cmdRegoPaths []string
 	cmdRules     []string
+	cmdCloud     *bool
 )
 
 func check(err error) {
@@ -20,8 +26,46 @@ func check(err error) {
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "upe",
+	Use:   "unified-policy-engine [-d <rules/metadata>...] [-r <rule ID>...] <input> [input...]",
 	Short: "Unified Policy Engine",
+	Run: func(cmd *cobra.Command, args []string) {
+		selectedRules := map[string]bool{}
+		for _, k := range cmdRules {
+			selectedRules[k] = true
+		}
+		providers := []data.Provider{}
+		for _, path := range cmdRegoPaths {
+			providers = append(providers, data.LocalProvider(path))
+		}
+		options := &upe.EngineOptions{
+			Providers: providers,
+			RuleIDs:   selectedRules,
+		}
+		inputType := loader.Auto
+		if *cmdCloud {
+			inputType = loader.TfRuntime
+		}
+		configLoader := loader.LocalConfigurationLoader(loader.LoadPathsOptions{
+			Paths:       args,
+			InputTypes:  []loader.InputType{inputType},
+			NoGitIgnore: false,
+			IgnoreDirs:  false,
+		})
+		loadedConfigs, err := configLoader()
+		check(err)
+
+		states := loadedConfigs.ToStates()
+		ctx := context.Background()
+		engine, err := upe.NewEngine(ctx, options)
+		check(err)
+
+		results, err := engine.Eval(ctx, states)
+		check(err)
+
+		bytes, err := json.MarshalIndent(results, "  ", "  ")
+		check(err)
+		fmt.Fprintf(os.Stdout, "%s\n", string(bytes))
+	},
 }
 
 func Execute() error {
@@ -29,6 +73,7 @@ func Execute() error {
 }
 
 func init() {
+	cmdCloud = rootCmd.PersistentFlags().Bool("cloud", false, "Causes inputs to be interpreted as runtime state from Snyk Cloud.")
 	rootCmd.PersistentFlags().StringSliceVarP(&cmdRegoPaths, "data", "d", cmdRegoPaths, "Rego paths to load")
 	rootCmd.PersistentFlags().StringSliceVarP(&cmdRules, "rule", "r", cmdRules, "Select specific rules")
 }
