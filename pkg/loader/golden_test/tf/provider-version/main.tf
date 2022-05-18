27b8ab6876d9312a893f0e3d42d731b8f122d9ae
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2022-05-18 15:16:53 +0200

    [CLOUD-200] Test provider aliases for versioning

--- pkg/loader/golden_test/tf/provider-version/main.tf
+++ pkg/loader/golden_test/tf/provider-version/main.tf
@@ -2,10 +2,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "aws" {
+  alias = "west"
+  region = "us-west-1"
+}
+
 terraform {
   required_providers {
     aws = {
-      version = "~> 3.0.0"
+      version = "~> 4.0.0"
     }
   }
 }
@@ -15,13 +20,21 @@ resource "aws_kms_key" "key" {
   deletion_window_in_days = 10
 }
 
-resource "aws_s3_bucket" "bucket" {
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.key.arn
-        sse_algorithm     = "aws:kms"
-      }
+resource "aws_s3_bucket" "bucket1" {
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  provider = aws.west
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket2" {
+  provider = aws.west
+  bucket = aws_s3_bucket.bucket2.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.key.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
