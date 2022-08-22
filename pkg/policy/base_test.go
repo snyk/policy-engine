3a867b6c73b97cd6f2884a51805d99ede6cf6b94
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2022-08-22 13:34:34 +0200

    [CLOUD-566] remove deprecated markdown-style references

--- pkg/policy/base_test.go
+++ pkg/policy/base_test.go
@@ -7,51 +7,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// TestParseMetadataReferences can be removed once we don't support the markdown
-// format anymore.
-func TestParseMetadataReferences(t *testing.T) {
-	meta1 := Metadata{}
-	err := json.Unmarshal([]byte(`{
-	"references": "[Some doc](https://example.com)"
-}`), &meta1)
-	assert.NoError(t, err)
-	assert.Equal(t,
-		meta1.parseMetadataReferences(),
-		map[string][]MetadataReference{
-			"general": {
-				{
-					Title: "Some doc",
-					URL:   "https://example.com",
-				},
-			},
-		},
-	)
-
-	meta2 := Metadata{}
-	err = json.Unmarshal([]byte(`{
-	"references": {
-		"general": [
-			{
-				"title": "Some doc",
-				"url": "https://example.com"
-			}
-		]
-	}
-}`), &meta2)
-	assert.NoError(t, err)
-	assert.Equal(t,
-		meta2.parseMetadataReferences(),
-		map[string][]MetadataReference{
-			"general": {
-				{
-					Title: "Some doc",
-					URL:   "https://example.com",
-				},
-			},
-		},
-	)
-}
-
 // TestReferencesFor tests that we obtain only the expected references.
 func TestReferencesFor(t *testing.T) {
 	meta1 := Metadata{}
