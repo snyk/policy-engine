b6817d898872b6bd32d2d14c0a8ddc4137cc7722
Jasper Van der Jeugt <100228594+jaspervdj-snyk@users.noreply.github.com>
2022-09-19 15:26:57 +0200

    [CLOUD-673] Add postprocess.ApplyCustomSeverities (#126)

---
+++ pkg/postprocess/custom_severities_test.go
@@ -0,0 +1,87 @@
+package postprocess
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/snyk/policy-engine/pkg/models"
+)
+
+func TestApplyCustomSeverities(t *testing.T) {
+	namespace := "golden_test/tfplan/resource-changes/plan.json"
+	in := models.Results{Results: []models.Result{{
+		RuleResults: []models.RuleResults{
+			{
+				Id: "SNYK-ABC-01",
+				Results: []models.RuleResult{
+					{
+						Passed:            false,
+						ResourceId:        "aws_s3_bucket.bucket",
+						ResourceType:      "aws_s3_bucket",
+						ResourceNamespace: namespace,
+						Severity:          "High",
+					},
+				},
+			},
+			{
+				Id: "SNYK-ABC-02",
+				Results: []models.RuleResult{
+					{
+						Passed:            false,
+						ResourceId:        "aws_s3_bucket.bucket",
+						ResourceType:      "aws_s3_bucket",
+						ResourceNamespace: namespace,
+						Severity:          "High",
+					},
+				},
+			},
+			{
+				Id: "SNYK-ABC-03",
+				Results: []models.RuleResult{
+					{
+						Passed:            false,
+						ResourceId:        "aws_s3_bucket.bucket",
+						ResourceType:      "aws_s3_bucket",
+						ResourceNamespace: namespace,
+						Severity:          "High",
+					},
+				},
+			},
+		},
+	}}}
+	customSeverities := CustomSeverities{
+		"SNYK-ABC-01": "None",
+		"SNYK-ABC-02": "Low",
+	}
+	expected := models.Results{Results: []models.Result{{
+		RuleResults: []models.RuleResults{
+			{
+				Id: "SNYK-ABC-02",
+				Results: []models.RuleResult{
+					{
+						Passed:            false,
+						ResourceId:        "aws_s3_bucket.bucket",
+						ResourceType:      "aws_s3_bucket",
+						ResourceNamespace: namespace,
+						Severity:          "Low",
+					},
+				},
+			},
+			{
+				Id: "SNYK-ABC-03",
+				Results: []models.RuleResult{
+					{
+						Passed:            false,
+						ResourceId:        "aws_s3_bucket.bucket",
+						ResourceType:      "aws_s3_bucket",
+						ResourceNamespace: namespace,
+						Severity:          "High",
+					},
+				},
+			},
+		},
+	}}}
+	ApplyCustomSeverities(&in, customSeverities)
+	assert.Equal(t, expected, in)
+}
