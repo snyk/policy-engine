c9453322eb775e911f7a7fe83d7e4cdee61dd260
Jason Lynch <jason.lynch@snyk.io>
2023-03-16 09:20:56 -0400

    refactor: replace "."s with "-"s in cloud args and use NewClientFromEnv

--- cmd/cloud.go
+++ cmd/cloud.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"context"
-	"os"
 
 	"github.com/snyk/policy-engine/pkg/input"
 	"github.com/snyk/policy-engine/pkg/input/cloudapi"
@@ -23,15 +22,15 @@ type cloudOptions struct {
 }
 
 func (c *cloudOptions) addFlags(cmd *cobra.Command) {
-	cmd.PersistentFlags().StringVar(&c.OrgID, "cloud.org", "", "Cloud organization ID (required to fetch cloud resources)")
-	cmd.PersistentFlags().StringSliceVar(&c.EnvIDs, "cloud.env", nil, "Cloud environment IDs (optional, can be specified multiple times)")
-	cmd.PersistentFlags().StringSliceVar(&c.ResourceTypes, "cloud.resource-type", nil, "Cloud resource types (optional, can be specified multiple times)")
-	cmd.PersistentFlags().StringSliceVar(&c.ResourceIDs, "cloud.resource-id", nil, "Cloud resource IDs (optional, can be specified multiple times)")
-	cmd.PersistentFlags().StringSliceVar(&c.NativeIDs, "cloud.native-id", nil, "Cloud resource native IDs, e.g. AWS resource ARNs (optional, can be specified multiple times)")
-	cmd.PersistentFlags().StringSliceVar(&c.IDs, "cloud.id", nil, "Cloud resource UUIDs (optional, can be specified multiple times)")
-	cmd.PersistentFlags().StringSliceVar(&c.Platforms, "cloud.platform", nil, "Cloud platforms, e.g. aws, azure (optional, can be specified multiple times)")
-	cmd.PersistentFlags().StringSliceVar(&c.Names, "cloud.name", nil, "Cloud resource names (optional, can be specified multiple times)")
-	cmd.PersistentFlags().StringSliceVar(&c.Locations, "cloud.location", nil, "Cloud resource locations, e.g. us-east-1 (optional, can be specified multiple times)")
+	cmd.PersistentFlags().StringVar(&c.OrgID, "cloud-org", "", "Cloud organization ID (required to fetch cloud resources)")
+	cmd.PersistentFlags().StringSliceVar(&c.EnvIDs, "cloud-env", nil, "Cloud environment IDs (optional, can be specified multiple times)")
+	cmd.PersistentFlags().StringSliceVar(&c.ResourceTypes, "cloud-resource-type", nil, "Cloud resource types (optional, can be specified multiple times)")
+	cmd.PersistentFlags().StringSliceVar(&c.ResourceIDs, "cloud-resource-id", nil, "Cloud resource IDs (optional, can be specified multiple times)")
+	cmd.PersistentFlags().StringSliceVar(&c.NativeIDs, "cloud-native-id", nil, "Cloud resource native IDs, e.g. AWS resource ARNs (optional, can be specified multiple times)")
+	cmd.PersistentFlags().StringSliceVar(&c.IDs, "cloud-id", nil, "Cloud resource UUIDs (optional, can be specified multiple times)")
+	cmd.PersistentFlags().StringSliceVar(&c.Platforms, "cloud-platform", nil, "Cloud platforms, e.g. aws, azure (optional, can be specified multiple times)")
+	cmd.PersistentFlags().StringSliceVar(&c.Names, "cloud-name", nil, "Cloud resource names (optional, can be specified multiple times)")
+	cmd.PersistentFlags().StringSliceVar(&c.Locations, "cloud-location", nil, "Cloud resource locations, e.g. us-east-1 (optional, can be specified multiple times)")
 }
 
 func (c *cloudOptions) enabled() bool {
@@ -39,11 +38,7 @@ func (c *cloudOptions) enabled() bool {
 }
 
 func getCloudStates(ctx context.Context, options cloudOptions) (*models.State, error) {
-	client, err := cloudapi.NewClient(cloudapi.ClientConfig{
-		URL:     os.Getenv("SNYK_API"),
-		Token:   os.Getenv("SNYK_TOKEN"),
-		Version: os.Getenv("API_VERSION"),
-	})
+	client, err := cloudapi.NewClientFromEnv()
 	if err != nil {
 		return nil, err
 	}
