90deac3ae2caa799d82ba697e1808383e2dd5b0d
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2023-03-29 17:43:22 +0200

    WIP

--- pkg/regobind/regobind.go
+++ pkg/regobind/regobind.go
@@ -1,1 +1,50 @@
 package regobind
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+
+	"github.com/open-policy-agent/opa/ast"
+)
+
+const tag = "rego"
+
+func Bind(src ast.Value, dst interface{}) error {
+	return bind(src, reflect.ValueOf(dst))
+}
+
+func bind(src ast.Value, dst reflect.Value) error {
+	ty := dst.Type()
+	if ty.Kind() == reflect.Struct {
+		if srcObject, ok := src.(ast.Object); ok {
+			for i := 0; i < ty.NumField(); i++ {
+				field := ty.Field(i)
+				regoFieldName, ok := field.Tag.Lookup(tag)
+				if ok {
+					fmt.Fprintf(os.Stderr, "binding field %s\n", regoFieldName)
+					regoFieldVal := srcObject.Get(ast.StringTerm(regoFieldName))
+					goFieldVal := dst.Field(i)
+					if err := bind(regoFieldVal.Value, goFieldVal); err != nil {
+						return err
+					}
+				}
+			}
+		} else {
+			return fmt.Errorf("Expected object")
+		}
+	} else if ty.Kind() == reflect.Pointer {
+		return bind(src, dst.Elem())
+	} else if ty.Kind() == reflect.Int {
+		if number, ok := src.(ast.Number); ok {
+			if n, ok := number.Int64(); ok {
+				dst.SetInt(n)
+			}
+		}
+	} else if ty.Kind() == reflect.String {
+		if str, ok := src.(ast.String); ok {
+			dst.SetString(str.String())
+		}
+	}
+	return nil
+}
--- pkg/regobind/regobind_test.go
+++ pkg/regobind/regobind_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBind(t *testing.T) {
+func TestBind1(t *testing.T) {
 	ctx := context.Background()
 	modules := map[string]*ast.Module{
 		"example.rego": ast.MustParseModule(`
@@ -29,9 +29,13 @@ people = [
 
 	query := ast.MustParseBody(`x = data.example.people[_].age + 1`)
 
-	q := topdown.NewQuery(query).
+	qc := compiler.QueryCompiler()
+	cq, err := qc.Compile(query)
+	assert.NoError(t, err)
+
+	q := topdown.NewQuery(cq).
 		WithCompiler(compiler)
-	err := q.Iter(ctx, func(qr topdown.QueryResult) error {
+	err = q.Iter(ctx, func(qr topdown.QueryResult) error {
 		fmt.Fprintf(os.Stderr, "============================\n")
 		for k, term := range qr {
 			fmt.Fprintf(os.Stderr, "TERM %s: %s\n", k, term.String())
@@ -44,9 +48,51 @@ people = [
 		for i, expr := range query {
 			fmt.Fprintf(os.Stderr, "EXPR %d: %s\n", i, expr.String())
 			if expr.Generated {
-    			continue
+				continue
 			}
 		}
+		return nil
+	})
+	assert.NoError(t, err)
+	t.Fatalf("%s", "bad stuff happened")
+}
+
+type Person struct {
+	Name string `rego:"name"`
+	Age  int    `rego:"age"`
+}
+
+func TestBind2(t *testing.T) {
+	ctx := context.Background()
+	modules := map[string]*ast.Module{
+		"example.rego": ast.MustParseModule(`
+package example
+
+people = [
+	{"name": "Sam", "age": 30},
+	{"name": "Kim", "age": 40},
+]`),
+	}
+
+	compiler := ast.NewCompiler()
+	compiler.Compile(modules)
+	assert.Len(t, compiler.Errors, 0)
+
+	query := ast.MustParseBody(`x = data.example.people[_]`)
+
+	qc := compiler.QueryCompiler()
+	cq, err := qc.Compile(query)
+	assert.NoError(t, err)
+
+	q := topdown.NewQuery(cq).
+		WithCompiler(compiler)
+	err = q.Iter(ctx, func(qr topdown.QueryResult) error {
+		fmt.Fprintf(os.Stderr, "============================\n")
+		var person Person
+		err = Bind(qr[ast.Var("x")].Value, &person)
+		assert.NoError(t, err)
+
+		fmt.Fprintf(os.Stderr, "BOUND: %v\n", person)
 		return nil
 	})
 	assert.NoError(t, err)
