12821edb7f9c9daadaef9e1f883d3d4109465afe
Jasper Van der Jeugt <jasper.vanderjeugt@snyk.io>
2023-08-16 12:30:23 +0200

    add test

---
+++ pkg/policy/input_resolver_test.go
@@ -0,0 +1,35 @@
+package policy
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/snyk/policy-engine/pkg/models"
+)
+
+func TestInputResolverDeterministic(t *testing.T) {
+	resource1 := models.ResourceState{
+		Id:           "bucket_1",
+		ResourceType: "aws_s3_bucket",
+	}
+	resource2 := models.ResourceState{
+		Id:           "bucket_2",
+		ResourceType: "aws_s3_bucket",
+	}
+	input := models.State{
+		Resources: map[string]map[string]models.ResourceState{
+			"aws_s3_bucket": map[string]models.ResourceState{
+				resource1.Id: resource1,
+				resource2.Id: resource2,
+			},
+		},
+	}
+	resolver := newInputResolver(&input)
+	result, err := resolver.resolve(context.Background(), ResourcesQuery{
+		ResourceType: "aws_s3_bucket",
+	})
+	require.NoError(t, err)
+	require.Equal(t, []models.ResourceState{resource1, resource2}, result.Resources)
+}
